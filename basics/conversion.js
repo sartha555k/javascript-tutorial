2be3329f5406bead103e1dcf20668ecb9b9bb6ef
sartha555k <119329539+sartha555k@users.noreply.github.com>
2024-08-23 12:39:10 +0000

    conversions in datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,7 @@
+let score = "10"
+console.log(typeof(score));
+let valueNumber = Number(score)
+console.log(valueNumber)
+let nameis = "sarthak"
+let booleanname = Boolean(nameis)
+console.log(booleanname);
